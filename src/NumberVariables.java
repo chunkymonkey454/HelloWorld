7208c24542e7daf51104df4da06802618a3f2c70
logan <logan.rm.cooper07@gmail.com>
2026-09-11 11:41:39 -0400

    Add new project

---
+++ src/NumberVariables.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class NumberVariables {
+    public static void main(String[] args) {
+        int numOfItems = 10;
+        double itemPrice = 3.5;
+        double totalValue = itemPrice * numOfItems;
+        System.out.println("Total value is " + totalValue);
+
+    }
+}
