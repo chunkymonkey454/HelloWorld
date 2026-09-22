68092feb307527e78861a57e8073a4dd640c41a0
logan <logan.rm.cooper07@gmail.com>
2026-09-21 20:28:44 -0400

    evenOdd

---
+++ src/evenOdd.java
@@ -0,0 +1,19 @@
+public class evenOdd {
+    public static void main(String [] args){
+boolean isNum = true;
+boolean isNotNum = false;
+
+int num =15;
+
+boolean isOdd = num % 2 != 0;
+
+boolean isEven = num % 2 ==0;
+
+        System.out.println("Is " +num + " an odd number " + isOdd);
+        System.out.println("Is "+num+" an even number "+isEven);
+        System.out.println("The number is 15 is odd");
+
+
+    }
+
+}
